a25e1bc54e5945857a3514aad2a887ddbd39c17e
radley wells <radleywells@gmail.com>
2022-08-06 17:41:49 +0100

    Back end for delete

--- backend/src/main/java/com/spektr/server/controller/RecipeController.java
+++ backend/src/main/java/com/spektr/server/controller/RecipeController.java
@@ -2,6 +2,7 @@ package com.spektr.server.controller;
 
 import com.spektr.server.model.Recipe;
 import com.spektr.server.repository.RecipeRepository;
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -55,5 +57,15 @@ public class RecipeController {
 
         Recipe updatedRecipe = _recipeRepository.save(recipeDetails);
         return ResponseEntity.ok(updatedRecipe);
+    }
+
+    @DeleteMapping("recipes/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteRecipe(@PathVariable long id) {
+        Recipe recipe = _recipeRepository.findById(id)
+                                         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find recipe with id " + id));
+
+        _recipeRepository.delete(recipe);
+
+        return ResponseEntity.ok(Map.of("deleted", true));
     }
 }
