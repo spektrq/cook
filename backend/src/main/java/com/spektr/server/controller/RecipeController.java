245dc852858570b20d9725b08fd8a0979ed0e590
radley wells <radleywells@gmail.com>
2022-08-12 12:02:13 +0100

    Something in backend

--- backend/src/main/java/com/spektr/server/controller/RecipeController.java
+++ backend/src/main/java/com/spektr/server/controller/RecipeController.java
@@ -2,7 +2,6 @@ package com.spektr.server.controller;
 
 import com.spektr.server.model.Recipe;
 import com.spektr.server.repository.RecipeRepository;
-import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
