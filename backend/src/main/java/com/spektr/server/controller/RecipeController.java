fe4b98f83eefd0e366d716601353bf6e5ed2f7d2
radley wells <radleywells@gmail.com>
2023-01-21 18:49:55 +0000

    Fix frontend -> backend

--- backend/src/main/java/com/spektr/server/controller/RecipeController.java
+++ backend/src/main/java/com/spektr/server/controller/RecipeController.java
@@ -15,7 +15,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("api/")
 public class RecipeController {
