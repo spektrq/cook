6f85ae3f24018c5aee244d49fc0421c1afc65451
radleywells <@radleywells@gmail.com>
2023-04-21 20:13:02 +0100

    more progress on recipe search

--- frontend/src/pages/SearchRecipe.js
+++ frontend/src/pages/SearchRecipe.js
@@ -1,35 +1,61 @@
 import React from "react";
 
 import SearchResultService from '../services/SearchResultService';
-import {Container, Card, Button} from 'react-bootstrap'
+import {Container, Card, Button, Form} from 'react-bootstrap'
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
-      result: ""
+      result: "",
+      ingredients: ""
     };
   }
 
-  setRandomTitleText = async () => {
-    await SearchResultService.getSearchResults('chicken').then((response) => 
-      {
-        console.log(response.data)
-        let titleData = response.data.map(d => d.title);
-        let randomTitle = titleData[Math.floor(Math.random() * titleData.length)];
-        this.setState({result: randomTitle});
-      }
-    )
+  setRecipeText = async () => {
+    if (this.state.ingredients) {
+      await SearchResultService.getSearchResults(this.state.ingredients).then((response) => 
+        {
+          console.log(response.data)
+          let titleData = response.data.map(d => d.title)
+
+          let randomTitle = titleData[Math.floor(Math.random() * titleData.length)]
+          while (titleData.length > 1 && this.state.result && randomTitle == this.state.result) {
+            let index = titleData.indexOf(randomTitle);
+            titleData.splice(index, 1)
+            randomTitle = titleData[Math.floor(Math.random() * titleData.length)]
+          }
+          
+          this.setState({result: randomTitle});
+        }
+      )
+    }
+  }
+  
+  setIngredients = (e) =>  {
+    this.setState({ingredients: e.target.value})
   }
 
   render() {
     return (
-      <Container>
+      <Container className='mt-5'>
+        <h1>
+          Idea Generator
+        </h1>
+        <br />
       <Card>
-        <div className='search'>
-          <Button variant="primary" onClick={this.setRandomTitleText}>Click me for ideas</Button>
-          <p>{this.state.result}</p>
-        </div>
+        <Card.Body>
+          <div className='search'>
+            <Card.Body>
+            <Form.Group className='mb-3'>
+              <Form.Label>Enter ingredients each separated by a comma:</Form.Label>
+              <Form.Control type='text' placeholder='Ingredients' onChange={this.setIngredients} value={this.state.ingredients}/>
+            </Form.Group>
+              <Card.Title>{this.state.result}</Card.Title>
+              <Button variant="primary" onClick={this.setRecipeText}>Click me for ideas</Button>
+            </Card.Body>
+          </div>
+        </Card.Body>
       </Card>
     </Container>
     );
